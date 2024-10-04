cef8e2d647e6df36e05e1401dc52f52b550e989b
gonzalo-villegas-curulla <gonzalo.villegas@hotmail.com>
2024-10-04 15:08:23 +0100

    .

--- Part01.py
+++ Part01.py
@@ -33,6 +33,36 @@ print('And the resulting uupdated kernel is: ', array)
 # %%
 gridsize  = 1024
 blocksize = 1024
+array = np.array([0.,1], dtype=np.float32)
 
+print('Initial array: ', array)
 print('Grid size: {}'.format(gridsize))
 print('Block size: {}'.format(blocksize))
+print('Total number of threads: {} (gsize x bsize)'.format(gridsize*blocksize))
+print('Run kernel with  gsize and bsize: kudakernel0[gsize,bsize](array)')
+cudakernel0[gridsize, blocksize](array)
+print('Updated array: ', array)
+
+# The result changes every time we run the kernel! Out of the total gsize*bsize threads,
+# two threads may read the array at the same time ==> same result
+# but one thread may write the output before another thread reads the array ==> different result
+
+# %% Now let's start indexing 
+
+@cuda.jit 
+def cudakernel1(array):
+    thread_pos = cuda.grid(1) # the "1" is a dimensional indexing request
+    # This gives a position pointer within the grid
+    array[thread_pos] += 0.5 
+
+array = np.array([0,1], dtype=np.float32)    
+print('Initial array: ', array)
+print('Launch the kernel1[1,2]()\n 1 block, 2 threads per block')
+cudakernel1[1,2](array)
+print('Array updated: ', array)
+
+print('Launching cudakernel1[1,1]() will only update\n the first cell of the array')
+cudakernel1[1,1](array)
+print('Updated array: ', array,' see?')
+
+
