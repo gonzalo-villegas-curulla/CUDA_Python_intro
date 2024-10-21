93509ed8102190b44951d12c6ba8de2e96097350
gonzalo-villegas-curulla <gonzalo.villegas@hotmail.com>
2024-10-21 11:04:58 +0100

    .

---
+++ Test_GPUnetworkx.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import networkx as nx
+import nx_cugraph as nxcg
+
+
+url = "https://data.rapids.ai/cugraph/datasets/cit-Patents.csv"
+df = pd.read_csv(url, sep=" ", names=["src", "dst"], dtype="int32")
+G = nx.from_pandas_edgelist(df, source="src", target="dst")
+# G = nx.Graph()
+
+# populate the graph
+#  ...
+
+nxcg_G = nxcg.from_networkx(G)             # conversion happens once here
+nx.betweenness_centrality(nxcg_G, k=1000)  # nxcg Graph type causes cugraph backend
+                                           # to be used, no conversion necessary
